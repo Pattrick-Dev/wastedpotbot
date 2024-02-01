ad2d5ea862abe4fb4458f1ddbe4ec8a65e6ac6cc
Pattrick-Dev <speedupmusicchannel@gmail.com>
2024-02-01 19:38:52 +0000

    removed unused function

--- index.js
+++ index.js
@@ -319,39 +319,6 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
-function updateBotNickname(isPaused) {
-  // Update the bot's nickname to indicate whether it's paused or not
-  client.guilds.cache.first().id;
-  const botUserId = "889320148643749899";
-  const pauseEmoji = "\u23F8";
-  const newNickname = isPaused ? `${pauseEmoji} wastedpotbot` : "wastedpotbot";
-
-  // Find the guild
-  const guild = client.guilds.cache.first();
-
-  if (!guild) {
-    console.error("Guild not found.");
-    return;
-  }
-
-  // Find the bot's member within the guild using its user ID
-  const botMember = guild.members.cache.get(botUserId);
-
-  if (!botMember) {
-    console.error("Bot member not found in the guild.");
-    return;
-  }
-
-  // Change the bot's nickname
-  botMember
-    .setNickname(newNickname)
-    .then((updatedMember) => {
-      console.log(`Bot's nickname updated to ${updatedMember.displayName}`);
-    })
-    .catch((error) => {
-      console.error("Error changing nickname:", error);
-    });
-}
 
 client.login(token);
 keepAlive();
